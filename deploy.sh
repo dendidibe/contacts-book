6dddfdafe39e898155625bdeb8c9f05c9a3436a5
dendidibe <mogilyk32@gmail.com>
2020-10-15 19:48:36 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,10 @@ set -e
 # build
 npm run build
 # navigate into the build output directory
-cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'Deploy'
 git push -f git@github.com:dendidibe/contacts-book.git master:gh-pages
 cd -
